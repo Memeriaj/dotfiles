fd8fe1ddf51876f24efcc34e08bcd63c92c84a9b
Alex Memering <alex@shortwave.com>
2021-08-25 22:44:45 -0700

    Updating gcloud paths

--- zsh/zshrc.d/gcloud.sh
+++ zsh/zshrc.d/gcloud.sh
@@ -1,4 +1,9 @@
-# Updates PATH for the Google Cloud SDK.
-if [ -f '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc' ]; then
-    . '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc';
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/alexmemering/google-cloud-sdk/path.zsh.inc' ]; then 
+  . '/Users/alexmemering/google-cloud-sdk/path.zsh.inc'; 
+fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/alexmemering/google-cloud-sdk/completion.zsh.inc' ]; then 
+  . '/Users/alexmemering/google-cloud-sdk/completion.zsh.inc'; 
 fi
